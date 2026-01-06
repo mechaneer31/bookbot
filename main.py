d1d1c9048442529efc4de87846d78fba32d693d7
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-01-06 15:30:33 -0800

    I did a terrible job of tracking my changes and work in progress. This is Ch2 completed.

---
+++ main.py
@@ -0,0 +1,16 @@
+from stats import get_num_words
+
+
+
+def main():
+    #book_num_words, book_char_dict = get_num_words("/home/chris/workspace/github.com/mechaneer31/bookbot/books/frankenstein.txt")
+
+    book_num_words, book_char_dict = get_num_words("/home/chris/workspace/github.com/mechaneer31/bookbot/books/test_book.txt")
+
+
+    print(f"Found {book_num_words} total words")
+    print(book_char_dict)
+    
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,60 @@
+def get_num_words(path_to_file):
+    
+    #get the book text
+    with open(path_to_file) as f:
+        #print(f"debugging: path_to_file is: {path_to_file}")
+
+        #file_contents has all the words of text
+        file_contents = f.read()
+        #print(f"debugging: file_contents: {file_contents}")
+
+        #split book into individual words
+        individual_words = file_contents.split()
+        #print(f"debugging: all words should be split: {individual_words}")
+
+
+        #get the word count
+        total_words = len(individual_words)
+
+
+        #create empty list to store lowercase words
+        all_lowercase_words = []
+
+        #make all words lowercase
+        for word in individual_words:
+            lowercase_word = word.lower()
+            all_lowercase_words.append(lowercase_word)
+            all_lowercase_words_count = len(all_lowercase_words)
+            #print(f"debugging: all_lowercase_words count: {all_lowercase_words_count}")
+            
+        #print(f"debugging: all words should be lowercase. {all_lowercase_words}")
+
+
+        #Set new empty list to break words into characters
+        separate_characters = []
+
+        for word in all_lowercase_words:
+            #print(f"debugging: checking lowercase words count. {word} {len(all_lowercase_words)}")
+            split_characters = list(word)
+            #print(f"debugging: split characters is: {split_characters}")
+            separate_characters = separate_characters + split_characters
+            #print(f"debugging: separate_characters is: {separate_characters}")
+        
+        
+        
+        character_count_dict = {}
+
+
+        for character_list_item in separate_characters:
+            if character_list_item in character_count_dict:
+                character_count_dict[character_list_item] += 1
+                #print(f"debugging: character {char} exists in dictionary, char value should increase by 1: {character_count_dict}")
+            
+            else:
+                character_count_dict[character_list_item] = 1
+
+        #print(f"debugging: character count dict is: {character_count_dict}")
+                       
+
+        return total_words, character_count_dict
+
