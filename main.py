60ccc157d1e6eefd8a2a19fce051552b6f3f475b
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-01-07 04:59:37 -0800

    added CLI argument check and usage notification. program activated by CLI filepath now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import get_num_words, revamp_dictionary
-
+import sys
 
 
 
@@ -7,13 +7,23 @@ from stats import get_num_words, revamp_dictionary
 
 
 def main():
-    book_num_words, book_char_dict = get_num_words("/home/chris/workspace/github.com/mechaneer31/bookbot/books/frankenstein.txt")
-    #book_num_words, book_char_dict = get_num_words("/home/chris/workspace/github.com/mechaneer31/bookbot/books/test_book.txt")
+
+    #print("Usage: python3 main.py <path_to_book>")
+    #input("Choose book to open: ")
     
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    book_file_path = sys.argv[1]
+
+    book_num_words, book_char_dict = get_num_words(book_file_path)
+    #book_num_words, book_char_dict = get_num_words("/home/chris/workspace/github.com/mechaneer31/bookbot/books/test_book.txt")
+
     char_list = revamp_dictionary(book_char_dict)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_file_path}...")
     print("----------- Word Count ----------")
     print(f"Found {book_num_words} total words")
     print("--------- Character Count -------")
