6d9363cf5e96d5e48c1e69f9b9ecbb470bb54021
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-01-07 06:10:11 -0800

    code is complete.  removed all debugging print statements and one unused variable. completed commenting code

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ import sys
 
 def main():
 
-    #print("Usage: python3 main.py <path_to_book>")
-    #input("Choose book to open: ")
-    
+    #file path to book required to run code.  If user tries to just run main.py this will instruct them that they need to add a book file path
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-        
+
+    #book file path gets pulled from sys.arg and put in program to be run    
     book_file_path = sys.argv[1]
-
+    #running function get_num_words form stats.py
     book_num_words, book_char_dict = get_num_words(book_file_path)
-    #book_num_words, book_char_dict = get_num_words("/home/chris/workspace/github.com/mechaneer31/bookbot/books/test_book.txt")
-
+    #running function revamp_dictionary form stats.py
     char_list = revamp_dictionary(book_char_dict)
 
+    #create an ouput format showing number of words in book and all alphabetical characters sorted from most to least
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_file_path}...")
     print("----------- Word Count ----------")
--- stats.py
+++ stats.py
@@ -1,77 +1,65 @@
+#function that gets the number of words from a book and creates a dictionary with all characters found in book paired to number of ocurrences
 def get_num_words(path_to_file):
     
     #get the book text
     with open(path_to_file) as f:
-        #print(f"debugging: path_to_file is: {path_to_file}")
-
+        
         #file_contents has all the words of text
         file_contents = f.read()
-        #print(f"debugging: file_contents: {file_contents}")
-
+        
         #split book into individual words
         individual_words = file_contents.split()
-        #print(f"debugging: all words should be split: {individual_words}")
-
 
         #get the word count
         total_words = len(individual_words)
-
-
-        #create empty list to store lowercase words
+    
+        #initialize empty list to store lowercase words
         all_lowercase_words = []
 
-        #make all words lowercase
+        #convert all letters in words to lowercase if they are not already
         for word in individual_words:
             lowercase_word = word.lower()
             all_lowercase_words.append(lowercase_word)
-            all_lowercase_words_count = len(all_lowercase_words)
-            #print(f"debugging: all_lowercase_words count: {all_lowercase_words_count}")
-            
-        #print(f"debugging: all words should be lowercase. {all_lowercase_words}")
-
-
-        #Set new empty list to break words into characters
+         
+        #initialize empty list to break words into characters
         separate_characters = []
 
-        for word in all_lowercase_words:
-            #print(f"debugging: checking lowercase words count. {word} {len(all_lowercase_words)}")
-            split_characters = list(word)
-            #print(f"debugging: split characters is: {split_characters}")
-            separate_characters = separate_characters + split_characters
-            #print(f"debugging: separate_characters is: {separate_characters}")
+        #loop through each word to split the words into their individual characters
+        for word in all_lowercase_words:            
+            split_characters = list(word)            
+            separate_characters = separate_characters + split_characters        
         
-        
-        
+        #initalize empty character count dictionary
         character_count_dict = {}
 
-
+        #loop through the separate characters list; if character already added to dictionary then just add an occurence, otherwise add character and set to value 1
         for character_list_item in separate_characters:
             if character_list_item in character_count_dict:
-                character_count_dict[character_list_item] += 1
-                #print(f"debugging: character {char} exists in dictionary, char value should increase by 1: {character_count_dict}")
+                character_count_dict[character_list_item] += 1                
             
             else:
-                character_count_dict[character_list_item] = 1
-
-        #print(f"debugging: character count dict is: {character_count_dict}")
-                       
+                character_count_dict[character_list_item] = 1                       
 
         return total_words, character_count_dict
+    
 
 
+#helper function for sorting in revamp_dictionary function
 def sort_on(items):
     return items["num"]
 
 
+
+#function to revamp the dictionary to only return alphabetical characters and create a list of dictionaries of format {"char": "", "num", num}
 def revamp_dictionary(char_count_dict):
-    
+    #initalize empty list to store dictionaries in
     dict_revamp_list = []
 
+    #loop through dictionary data called into function
     for char_count_key in char_count_dict:
        
        #remove any non-alphanumeric pairs
-       if char_count_key.isalpha():
-        #print(f"debugging: char_count_pair is: {char_count_key}")
+       if char_count_key.isalpha():        
 
         #initialize temp list to append dict_revamp_list
         temp_list = []
@@ -80,38 +68,29 @@ def revamp_dictionary(char_count_dict):
         temp_list.append("char")
         temp_list.append(char_count_key)
         temp_list.append("num")
-        temp_list.append(char_count_dict[char_count_key])
-
-        #print(f"debugging: temp list is: {temp_list}")
+        temp_list.append(char_count_dict[char_count_key])        
 
         #add the temp list to dict_revamp_list 
-        dict_revamp_list.append(temp_list)
-        #print(f"debugging: dict_revamp_list is: {dict_revamp_list}")
+        dict_revamp_list.append(temp_list)    
 
-
-    #create empty new dictionary for new key/value pairs
-    #dict_revamp_dict = {}
-
+    #initialize empty list to place new revamped dictionaries into
     revamp_dict_list = []
 
     #loop through dict_revamp_list to add new key/value pairs to dictionary
     for list in dict_revamp_list:
         
-        #create temp_dict for loop to add to list
+        #initialize temp_dict for loop to add to list
         temp_dict = {}
-
-        #print(f"debugging: list is: {list}")
+        
+        #get values to place into temp dictionary for each loop iteration to be stored in the list of dictionaries
         temp_dict[list[0]] = list[1]
-        temp_dict[list[2]] = list[3]
+        temp_dict[list[2]] = list[3]        
 
-        #print(f"debugging: dict_revamp_dict is: {temp_dict}")
-
+        #add the temporary dictionary created to the list of dictionaries
         revamp_dict_list.append(temp_dict)
 
-    #print(revamp_dict_list)
-
-    revamp_dict_list.sort(reverse = True, key=sort_on)
-    #print(f"debugging: revamp dict list should be sorted: {revamp_dict_list}")
+    #sort the revamped list of dictionaries to sort based on character count values from largest to smallest
+    revamp_dict_list.sort(reverse = True, key=sort_on)    
 
     return revamp_dict_list
 
